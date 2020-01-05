2dc806e7a6ef6c3cfd61c2814c44ff43ed55dea4
Andree Klattenhoff <mail@andr.ee>
2020-01-05 19:55:46 +0100

    update WireGuard version

--- packages/network/wireguard/package.mk
+++ packages/network/wireguard/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="wireguard"
-PKG_VERSION="0.0.20191012"
-PKG_SHA256="93573193c9c1c22fde31eb1729ad428ca39da77a603a3d81561a9816ccecfa8e"
+PKG_VERSION="0.0.20191219"
+PKG_SHA256="5aba6f0c38e97faa0b155623ba594bb0e4bd5e29deacd8d5ed8bda8d8283b0e7"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://www.wireguard.com"
 PKG_URL="https://git.zx2c4.com/WireGuard/snapshot/WireGuard-$PKG_VERSION.tar.xz"
