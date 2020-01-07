06ed70a0d7a0bede51a787db6ac449ad379c5a96
Andree Klattenhoff <mail@andr.ee>
2020-01-08 00:47:51 +0100

    add libseccomp package

---
+++ packages/devel/libseccomp/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libseccomp"
+PKG_VERSION="2.4.2"
+PKG_SHA256="b54f27b53884caacc932e75e6b44304ac83586e2abe7a83eca6daecc5440585b"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/seccomp/libseccomp"
+PKG_URL="https://github.com/seccomp/libseccomp/releases/download/v$PKG_VERSION/libseccomp-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="libseccomp"
+PKG_TOOLCHAIN="autotools"
